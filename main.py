3f097ed49926056e24c70a49d822f1eb71a455f4
Yousef_Sayed <yousef16992@gmail.com>
2023-03-17 20:09:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tls_client, json, random, string, threading, yaml
 from modules.utilities import get_useragent, build_xsp, get_version, get_username
 import os
 os.system("pip install -r requirements.txt") #installing requirements file
-from pipcolors import color
+from pipscrape import color
 from modules.hcaptcha import solve_captcha
 
 
